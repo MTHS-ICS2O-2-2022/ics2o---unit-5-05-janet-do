c4c1087afd646a153951513886b17aabe692319a
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-11 02:10:50 +0000

    sync

---
+++ go_app/main.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2020 Janet Do All rights reserved
+//
+// Created by: Janet Do
+// Created on: Sep 2020
+// This file determines the type of triangle based on input
+
+package main
+
+import (
+	"fmt"
+)
+
+func classifyTriangle(a, b, c int) string {
+	// Check if inputs are valid
+	if a <= 0 || b <= 0 || c <= 0 || a+b+c != 180 {
+		return "Invalid input: please enter valid lengths for all sides of the triangle that add up to 180 degrees"
+	}
+
+	// Process
+	if a == b && b == c {
+		return "The triangle is equilateral"
+	} else if a == b || b == c || c == a {
+		return "The triangle is isosceles"
+	} else {
+		return "The triangle is scalene"
+	}
+}
+
+func main() {
+	var a, b, c int
+
+	// Get user input for the lengths of the triangle
+	fmt.Print("Enter the length of the first side: ")
+	fmt.Scanln(&a)
+
+	fmt.Print("Enter the length of the second side: ")
+	fmt.Scanln(&b)
+
+	fmt.Print("Enter the length of the third side: \n")
+	fmt.Scanln(&c)
+
+	// Classify the triangle
+	classification := classifyTriangle(a, b, c)
+	fmt.Println(classification)
+}
+
--- js/script.js
+++ js/script.js
@@ -7,43 +7,55 @@
 "use strict"
 
 function calculate() {
-  // get input values from HTML form
-  const lengthA = parseFloat(document.getElementById("angle1").value)
-  const lengthB = parseFloat(document.getElementById("angle2").value)
-  const lengthC = parseFloat(document.getElementById("angle3").value)
+  // input
+  const lengthA = parseInt(document.getElementById("lengthA").value)
+  const lengthB = parseInt(document.getElementById("lengthB").value)
+  const lengthC = parseInt(document.getElementById("lengthC").value)
 
-  // check if input is valid
-  if (isNaN(lengthA) || isNaN(lengthB) || isNaN(lengthC)) {
-    document.getElementById("triangleType").innerHTML =
-      "Please enter valid numbers"
-    return
-  }  b
-  // using the cosine law
-  const angleA = Math.acos(
-    (lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)
-  ).toFixed(2)
-  const angleB = Math.acos(
-    (lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)
-  ).toFixed(2)
-  const angleC = Math.acos(
-    (lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)
-  ).toFixed(2)
-
-  // determine triangle type based on side lengths
-  let triangleType
-  if (lengthA === lengthB && lengthB === lengthC) {
-    triangleType = "equilateral"
-  } else if (
-    lengthA === lengthB ||
-    lengthA === lengthC ||
-    lengthB === lengthC
+  // Check if inputs are valid
+  if (
+    lengthA <= 0 ||
+    lengthB <= 0 ||
+    lengthC <= 0 ||
+    lengthA + lengthB + lengthC !== 180
   ) {
-    triangleType = "isosceles"
-  } else {
-    triangleType = "scalene"
+    // output
+    document.getElementById("answer").innerHTML =
+      "Invalid input: please enter valid lengths for all sides of the triangle that add up to 180 degrees"
+    return
   }
 
-  // display result in HTML page
-  document.getElementById("triangleType").innerHTML =
-    "This is a " + triangleType + " triangle"
+  const sumOfAngles = Math.round(
+    (Math.acos(
+      (lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)
+    ) +
+      Math.acos(
+        (lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)
+      ) +
+      Math.acos(
+        (lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)
+      )) *
+      (180 / Math.PI)
+  )
+
+  // process
+  if (lengthA === lengthB && lengthB === lengthC) {
+    // output
+    document.getElementById("answer").innerHTML = "The triangle is equilateral"
+  } else if (
+    lengthA === lengthB ||
+    lengthB === lengthC ||
+    lengthC === lengthA
+  ) {
+    // output
+    document.getElementById("answer").innerHTML = "The triangle is isosceles"
+  } else if (
+    lengthA !== lengthB &&
+    lengthB !== lengthC &&
+    lengthC !== lengthA
+  ) {
+    // output
+    document.getElementById("answer").innerHTML = "The triangle is scalene"
+  }
 }
+
